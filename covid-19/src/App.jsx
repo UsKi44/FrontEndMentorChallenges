fdf06323d2a75dd5cead137cc7891ad2e7e8ed06
Luka Phirtskhalava <lukaphirtskhalavaa5@gmail.com>
2021-09-26 21:22:06 +0400

    +breaking-bad

--- covid-19/src/App.jsx
+++ covid-19/src/App.jsx
@@ -3,14 +3,14 @@ import "./styles/App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Search from "./components/Search";
+import Countries from "./components/Countries";
 
 function App() {
   return (
     <Router className="App">
       <Header />
-      <Route exact path="/">
-        <Search />
-      </Route>
+      <Search />
+      <Countries />
     </Router>
   );
 }
--- covid-19/src/components/Search.jsx
+++ covid-19/src/components/Search.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "../styles/Search.css";
 
 function Search() {
+  useEffect(() => {
+    const searchCountry = document.getElementById("search");
+    searchCountry.addEventListener("input", (e) => {
+      const { value } = e.target;
+      const countryName = document.querySelectorAll(".countryName");
+
+      countryName.forEach((name) => {
+        console.log(name.innerText);
+        if (name.innerText.toLowerCase().includes(value.toLowerCase())) {
+          name.parentElement.style.display = "block";
+        } else {
+          name.parentElement.style.display = "none";
+        }
+      });
+    });
+  }, []);
+
   return (
     <div className="Search">
       <div className="search_cont">
