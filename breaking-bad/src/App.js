2bbd0ccf686aea278fc8d6c0d9e7ab07ad93cb98
UsKi44 <lukaphirtskhalavaa5@gmail.com>
2021-10-01 06:17:23 -0800

    first commit

--- breaking-bad/src/App.js
+++ breaking-bad/src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from "react"
+
+
 
 function App() {
+  const [name, setName] = useState("")
+  const [characters, setCharacters] = useState([])
+  const handleChanges = event => {    
+    setName(event.target.value)
+    // console.log(name)
+    event.preventDefault()
+  }
+
+  const url = `https://www.breakingbadapi.com/api/characters?name=${name}`
+  const getCharacters = async() => {
+    const response = await fetch(url)
+    const data = await response.json()
+    setCharacters(data)
+    console.log(characters)
+    
+  }
+  
+  useEffect(() => {
+    getCharacters();    
+    if (name ==="") {
+      document.getElementById("cards").style.display = "none"
+    }else {
+      document.getElementById("cards").style.display = "block"
+    }
+    
+    
+  },[url]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="search-comp">
+        <input type="text" placeholder="Search by name" className="search" value={name} onChange={handleChanges}/>
+      </div>
+      <div className="character_cards" id="cards">
+        {characters.map((info) => {
+          const {
+            char_id,
+            name,
+            nickname,
+            birthday,
+            portrayed,
+            status,
+            img,
+          } = info
+
+          return (
+            <div className="card" key={char_id}>
+              <img src={img} alt={name}/>
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
